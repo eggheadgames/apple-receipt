f6a39da81522d2d7350cb270a972554eae8f8e74
derpoliuk <derpoliuk@gmail.com>
2016-09-17 09:37:53 +0300

    Add Carthage support

---
+++ ReceiptKit/ReceiptKit.h
@@ -0,0 +1,19 @@
+//
+//  ReceiptKit.h
+//  ReceiptKit
+//
+//  Created by Stanislav Derpoliuk on 9/16/16.
+//  Copyright © 2016 Egghead Games LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ReceiptKit.
+FOUNDATION_EXPORT double ReceiptKitVersionNumber;
+
+//! Project version string for ReceiptKit.
+FOUNDATION_EXPORT const unsigned char ReceiptKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReceiptKit/PublicHeader.h>
+
+
